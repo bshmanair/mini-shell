0230e49bf57e91ed011c7efc358bd0f90fa409b9
bshmanair <71930306+bshmanair@users.noreply.github.com>
2025-03-27 15:13:31 +0000

    a

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(){
-    printf("Hello world");
+#include <string.h>
+int main()
+{
+    char input[100] = "";
+    do
+    {
+        printf("mini-shell>");
+        fgets(input, 100, stdin);           // accept first 100 characters
+        input[strcspn(input, "\n")] = '\0'; // remove trailing newline character
+        printf("%s", input);
+    } while (strcmp(input, "exit") != 0);
     return 0;
 }
