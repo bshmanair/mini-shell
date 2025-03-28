0b058db3b862268c0ca876d88f75fc6aa7d0a0f9
bshmanair <71930306+bshmanair@users.noreply.github.com>
2025-03-28 12:29:19 +0000

    Improvements and bug fixes

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include "DynamicArrays.h"
-#include "DynamicArrays.c"
 
 int main()
 {
+    int token_count = 0;
     char input[100] = "";
     char **tokens = NULL;
     do
@@ -21,41 +20,39 @@ int main()
         input[strcspn(input, "\n")] = '\0';      // remove trailing newline character
 
         // now making token array
-        char token = strtok(input, " "); // Points to first token
-        char **tokens = NULL;
+        char *token = strtok(input, " "); // Points to first token
 
         // Establish no. of tokens
-        int token_count = 1;
+        token_count = 1;
         for (int i = 0; input[i] != '\0'; i++)
         {
             if (input[i] == ' ')
                 token_count++;
         }
+        tokens = malloc(token_count * sizeof(char *));
 
         // Allocate memory then assign each token element in token array
-        for (int i = 0; i < token_count; i++)
+        int i = 0;
+        while (token != NULL && i < token_count)
         {
-            while (token != NULL)
-            {
-                size_t token_size = strlen(token);
-                tokens[i] = (char *)malloc(sizeof(char) * token_size);
-                tokens[i] = token;
-                token = strtok(input, " ");
-            }
+            size_t token_size = strlen(token);
+            tokens[i] = strdup(token);
+            token = strtok(NULL, " ");
+            i++;
         }
-        // Getting first command
-        char first[100] = "";
-        for (int i = 0; input[i] != ' ' || input[i] != '\0'; i++)
-        {
-            first[i] = input[i];
-        }
+        tokens[i] = NULL;
 
         // Process management
         pid_t process_id = fork();
         if (process_id == 0) // Child process
-            execvp(first, tokens);
+            execvp(tokens[0], tokens);
         else
-            break;
+            wait(NULL);
+
+        // Freeing all tokens
+        for (int i = 0; i < token_count; i++)
+            free(tokens[i]);
+        free(tokens);
     } while (strcmp(input, "exit") != 0);
 
     printf("\nProgram executed, exiting...\n");
