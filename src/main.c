1d0209edbbbf37d60cd495584f0f275cb92a3752
bshmanair <71930306+bshmanair@users.noreply.github.com>
2025-05-27 14:50:41 +0400

    Now prints user directories

--- src/main.c
+++ src/main.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main()
+int main(void)
 {
     char *input = NULL;
     size_t input_len = 0;
+    char *path;
     do
     {
+        path = getcwd(NULL,0);
+        if (path != NULL)
+        {
+            printf("\nYou are currently in: %s\n",path);
+            free(path);
+        }
+        else
+        {
+            perror("getcwd error");
+        }
+
+        
         printf("Enter prompt: ");
         if (getline(&input,&input_len,stdin) != -1)
         {
@@ -19,11 +33,12 @@ int main()
             input[0] = '\0';
         }
         printf("%s\n",input);
-
+        printf("%ld\n",input_len);
     }
     while(strcmp(input,"exit") != 0);
     printf("\n\n\nShell terminated. Exiting...\n\n\n");
 
     free(input);
+    free(path);
     return 0;
 }
